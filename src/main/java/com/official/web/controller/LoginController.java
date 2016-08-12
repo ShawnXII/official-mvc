7cc66998ddbf8d6656c55f7c85f4c83e1153b236
ShawnXII <wang492715@foxmail.com>
2016-08-12 19:09:00 +0800

    tj

--- src/main/java/com/official/web/controller/LoginController.java
+++ src/main/java/com/official/web/controller/LoginController.java
@@ -1,11 +1,17 @@
 package com.official.web.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.google.common.collect.Maps;
 
 /**
  * 登录/注册/找回密码相关页面和接口
@@ -28,10 +34,17 @@ public class LoginController extends BaseController{
 		return view;
 	}
 	
-	@RequestMapping(value = "/toLogin.htm",method = RequestMethod.POST)
-	public String login(HttpServletRequest request){
-		return "";
+	@RequestMapping(value = "/toLogin.htm",method = {RequestMethod.GET,RequestMethod.POST})
+	public String login(HttpServletRequest request, RedirectAttributes attr){
+		attr.addAttribute("test", "aaa");
+		return "redirect:/login/login.htm";
 	}
 	
-	
+	@RequestMapping(value = "/ajax.htm",method = {RequestMethod.GET,RequestMethod.POST})
+	@ResponseBody
+	public Map<String,Object> ajax(HttpServletRequest request){
+		Map<String,Object> map=Maps.newHashMap();
+		map.put("test", "测试ajax");		
+		return map;
+	}
 }
