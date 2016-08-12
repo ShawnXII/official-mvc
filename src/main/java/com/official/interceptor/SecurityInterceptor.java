2fa61d08c58f8b346a42e9eed0ba3c67604f2e56
ShawnXII <wang492715@foxmail.com>
2016-08-12 17:00:49 +0800

    提交

---
+++ src/main/java/com/official/interceptor/SecurityInterceptor.java
@@ -0,0 +1,37 @@
+package com.official.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 登录拦截器 
+ * @author ShawnXII
+ * @Version 1.0
+ */
+public class SecurityInterceptor implements HandlerInterceptor{
+	/*
+	 * 拦截器
+	 * (non-Javadoc)
+	 * @see org.springframework.web.servlet.HandlerInterceptor#preHandle(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object)
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+	
+	}
+
+}
